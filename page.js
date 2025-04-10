15bbdb9904e1404ec83026efe1951db43362db1e
Sarah <sah.winnermind@gmail.com>
2025-04-10 08:38:04 -0300

    Add files via upload

---
+++ page.js
@@ -0,0 +1,6 @@
+var doces = ["Chocolate", "Crème Brûlée", "Marshmallow", "Nutella", "Mousse de maracujá", 
+"Torta Holandesa", "Carolina", "Sonho", "Ouro branco", "Maria mole", "Bolo gelado",
+ "Ferrero Rocher", "Caramelo", "Doce de banana", "Paçoca"]
+ 
+var doceEscolhido = parseInt(prompt("Digite um número de 0 a 15"))
+document.write("Doce Escolhido:   " + doces[doceEscolhido])
